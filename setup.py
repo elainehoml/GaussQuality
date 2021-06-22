7d15c0dc593752c35f0aa3fe8a1ede3cb58a35ab
elainehoml <eml.ho@soton.ac.uk>
2021-06-22 14:52:37 +0100

    Moves out the old exe build

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='GaussQual',
-    version='0.1dev',
-    packages=['GaussQual',],
-    license='GNU GPLv2',
-    long_description=open('README.md').read(),
-)
