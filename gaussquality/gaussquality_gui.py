9d745caad0553d69ee16f66cc4d681dcf094eb0b
elainehoml <eml.ho@soton.ac.uk>
2021-06-23 18:25:14 +0100

    Increased font size and readability

--- gaussquality/gaussquality_gui.py
+++ gaussquality/gaussquality_gui.py
@@ -6,6 +6,7 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 from ttkthemes import ThemedTk
+from tkinter import font
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -18,7 +19,7 @@ import gaussquality_visuals
 class gaussquality_gui(tk.Frame):
 
     def __init__(self, root=None):
-        super().__init__(root)
+        super().__init__(root, padx=25, pady=25, bd=25)
         root.title("GaussQuality: Image Quality Assessment with Gaussian Mixture Models")
         self.root = root
         print("-------- GaussQuality --------")
@@ -35,83 +36,94 @@ class gaussquality_gui(tk.Frame):
         ttk.Label(self.root, image=self.logo).grid(row=0, column=0, rowspan=2)
         ttk.Label(self.root, 
                   text="GaussQuality: Image quality evaluation with Gaussian Mixture Models",
-                  font=(None, 15)
+                  font=("Helvetica", 15)
                   ).grid(row=0, column=1, columnspan=3)
         ttk.Label(self.root,
-                  text="University of Southampton, 2021. GNU GPL v3.0"
+                  text="University of Southampton, 2021. GNU GPL v3.0",
+                  style="TLabel"
                  ).grid(row=1, column=1, columnspan=3)
         
         # Load image directory, set save directory
         ttk.Label(self.root,
                   text="Step 1: Setup",
-                  font=(None, 12, "bold")
-                  ).grid(row=2, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=10)
+                  font=("Helvetica", 12, "bold")
+                  ).grid(row=2, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=5)
         ttk.Button(self.root,
                    text="Load an image sequence folder", 
-                   command=self.get_img_dir
-                   ).grid(row=3, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=10)
+                   command=self.get_img_dir,
+                   style="TButton"
+                   ).grid(row=3, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=5)
         ttk.Button(self.root,
                    text="Set save directory", 
-                   command=self.set_save_dir
-                   ).grid(row=3, column=2, columnspan=2, sticky="NWES", ipadx=10, ipady=10)
+                   command=self.set_save_dir,
+                   style="TButton"
+                   ).grid(row=3, column=2, columnspan=2, sticky="NWES", ipadx=10, ipady=5)
 
         # Choose number of components
         self.n_components = tk.IntVar()
         ttk.Label(self.root,
-                 text="Number of Gaussian components"
+                 text="Number of Gaussian components",
+                 style="TLabel"
                  ).grid(row=4, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                  textvariable=self.n_components
-                 ).grid(row=4, column=2, columnspan=2, sticky="NWES")
+                 ).grid(row=4, column=2, columnspan=2, sticky="NWS")
 
         # Choose z-percentage
         self.z_percentage = tk.DoubleVar(value=70.)
         ttk.Label(self.root,
-                  text="Percentage of stack to use in z"
+                  text="Percentage of stack to use in z",
+                  style="TLabel"
                   ).grid(row=5, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                   textvariable=self.z_percentage
-                  ).grid(row=5, column=2, columnspan=2, sticky="NWES")
+                  ).grid(row=5, column=2, columnspan=2, sticky="NWS")
 
         # Choose number of runs
         self.n_runs = tk.IntVar(value=10)
         ttk.Label(self.root,
-                  text="Number of slices to analyse"
+                  text="Number of slices to analyse",
+                  style="TLabel"
                   ).grid(row=6, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                   textvariable=self.n_runs
-                  ).grid(row=6, column=2, columnspan=2, sticky="NWES")
+                  ).grid(row=6, column=2, columnspan=2, sticky="NWS")
 
         # Choose mask percentage
         self.mask_percentage = tk.DoubleVar(value=100.)
         ttk.Label(self.root,
-                  text="Percentage of image to use in x-y"
+                  text="Percentage of image to use in x-y",
+                  style="TLabel"
                   ).grid(row=7, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                   textvariable=self.mask_percentage
-                  ).grid(row=7, column=2, columnspan=2, sticky="NWES")
+                  ).grid(row=7, column=2, columnspan=2, sticky="NWS")
 
         # Preview
         ttk.Button(self.root,
                    text="Preview", 
-                   command=self.preview
-                   ).grid(row=8, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=10)
+                   command=self.preview,
+                   style="TButton"
+                   ).grid(row=8, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=5)
 
         # Choose thresholds
         ttk.Label(self.root,
                   text="Step 2: Set optional parameters",
-                  font=(None, 12, "bold")
-                  ).grid(row=9, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=10)
+                  font=("Helvetica", 12, "bold")
+                  ).grid(row=9, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=5)
         self.lower_threshold = tk.DoubleVar()
         self.upper_threshold = tk.DoubleVar()
         ttk.Label(self.root,
-                  text="Lower"
+                  text="Lower",
+                  style="TLabel"
                  ).grid(row=10, column=2, sticky="W")
         ttk.Label(self.root,
-                  text="Upper"
+                  text="Upper",
+                  style="TLabel"
                  ).grid(row=10, column=3, sticky="W")
         ttk.Label(self.root, 
-                 text="Grey value thresholds. Ignored if no thresholding applied."
+                 text="Grey value thresholds. Ignored if no thresholding applied.",
+                 style="TLabel"
                  ).grid(row=11, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                   textvariable=self.lower_threshold
@@ -120,75 +132,85 @@ class gaussquality_gui(tk.Frame):
                   textvariable=self.upper_threshold
                   ).grid(row=11, column=3, sticky="NWS")
         ttk.Button(self.root,
-                   text="Update threshold", 
-                   command=self.apply_threshold
-                   ).grid(row=11, column=4, sticky="NWES")
+                   text="Update", 
+                   command=self.apply_threshold,
+                   style="TButton"
+                   ).grid(row=11, column=4, sticky="NWS")
 
         # Enter a filename to save images
         self.prefix = tk.StringVar(value="Image0")
         ttk.Label(self.root,
                  text="Image name",
+                 style="TLabel"
                  ).grid(row=12, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                  textvariable=self.prefix
-                 ).grid(row=12, column=2, columnspan=2, sticky="NWES")
+                 ).grid(row=12, column=2, columnspan=2, sticky="NWS")
         
         # Enter material names for plots
         self.material_names_entry = tk.StringVar()
         ttk.Label(self.root,
-                  text="Material names, separated with commas"
+                  text="Material names, separated with commas",
+                  style="TLabel"
                   ).grid(row=13, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                   textvariable=self.material_names_entry
-                  ).grid(row=13, column=2, columnspan=2, sticky="NWES")
+                  ).grid(row=13, column=2, columnspan=2, sticky="NWS")
         ttk.Button(self.root,
-                   text="Update material names",
-                   command=self.add_material_names
-                   ).grid(row=13, column=4, sticky="NWES")
+                   text="Update",
+                   command=self.add_material_names,
+                   style="TButton"
+                   ).grid(row=13, column=4, sticky="NWS")
 
         # Run GaussQuality
         ttk.Button(self.root,
                    text="Run GaussQuality (this could take a while)",
-                   command=self.run_gaussquality
-                   ).grid(row=14, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=10)
+                   command=self.run_gaussquality,
+                   style="TButton"
+                   ).grid(row=14, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=5)
 
         # Plot results
         ttk.Label(self.root,
                   text="Step 3: Plotting",
-                  font=(None, 12, "bold")
-                  ).grid(row=15, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=10)
+                  font=("Helvetica", 12, "bold")
+                  ).grid(row=15, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=5)
         ttk.Button(self.root,
                    text="Plot central image and histogram",
-                   command=self.plot_image_and_histo
-                   ).grid(row=16, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=10)
-        self.img_histo_icon = tk.PhotoImage(file="logo/img_and_histo_200x100.pnm")
-        ttk.Label(self.root,
-                  image=self.img_histo_icon
-                 ).grid(row=17, column=0, columnspan=2)
+                   command=self.plot_image_and_histo,
+                   style="TButton"
+                   ).grid(row=16, column=0, columnspan=2, sticky="NWS", ipadx=10, ipady=5)
+        # self.img_histo_icon = tk.PhotoImage(file="logo/img_and_histo_200x100.pnm")
+        # ttk.Label(self.root,
+        #           image=self.img_histo_icon
+        #          ).grid(row=17, column=0, columnspan=2)
         ttk.Button(self.root,
                    text="Plot slice variation",
-                   command=self.plot_slice_variation
-                   ).grid(row=16, column=2, columnspan=2, sticky="NWES", ipadx=10, ipady=10)
-        self.slice_var_icon = tk.PhotoImage(file="logo/slice_var_200x100.pnm")
-        ttk.Label(self.root,
-                  image=self.slice_var_icon
-                  ).grid(row=17, column=2, columnspan=2)
+                   command=self.plot_slice_variation,
+                   style="TButton"
+                   ).grid(row=16, column=2, columnspan=2, sticky="NWES", ipadx=10, ipady=5)
+        # self.slice_var_icon = tk.PhotoImage(file="logo/slice_var_200x100.pnm")
+        # ttk.Label(self.root,
+        #           image=self.slice_var_icon
+        #           ).grid(row=17, column=2, columnspan=2)
         
         # SNR and CNR calculation
         ttk.Label(self.root,
                   text="Step 4: SNR and CNR Calculation",
-                  font=(None, 12, "bold")
-                  ).grid(row=18, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=10)
+                  font=("Helvetica", 12, "bold")
+                  ).grid(row=18, column=0, columnspan=3, sticky="NWES", ipadx=10, ipady=5)
         self.background_mat = tk.IntVar()
         self.feature_mat = tk.IntVar()
         ttk.Label(self.root,
-                  text="Background Material"
+                  text="Background Material",
+                  style="TLabel"
                  ).grid(row=19, column=2, sticky="W")
         ttk.Label(self.root,
-                  text="Feature Material"
+                  text="Feature Material",
+                  style="TLabel"
                  ).grid(row=19, column=3, sticky="W")
         ttk.Label(self.root, 
-                 text="Material Gaussian numbers for calculating SNR and CNR"
+                 text="Material Gaussian numbers for calculating SNR and CNR",
+                 style="TLabel"
                  ).grid(row=20, column=0, columnspan=2, sticky="NWES")
         ttk.Entry(self.root,
                   textvariable=self.background_mat
@@ -197,13 +219,15 @@ class gaussquality_gui(tk.Frame):
                   textvariable=self.feature_mat
                   ).grid(row=20, column=3, sticky="NWS")
         ttk.Button(self.root,
-                   text="Update materials", 
-                   command=self.update_materials
-                   ).grid(row=20, column=4, sticky="NWES")
+                   text="Update", 
+                   command=self.update_materials,
+                   style="TButton"
+                   ).grid(row=20, column=4, sticky="NWS")
         ttk.Button(self.root,
                    text="Calculate SNR and CNR",
-                   command=self.calc_snr_cnr
-                   ).grid(row=21, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=10)
+                   command=self.calc_snr_cnr,
+                   style="TButton"
+                   ).grid(row=21, column=0, columnspan=2, sticky="NWES", ipadx=10, ipady=5)
         
 
     def get_img_dir(self):
@@ -379,7 +403,10 @@ def redirector(inputStr=""):
 
 root = ThemedTk(theme="black")
 style = ttk.Style()
-style.configure("TLabel", background="#555555")
+font_label = font.Font(size=11)
+style.configure("TLabel", background="#555555", font=font_label)
+font_button = font.Font(size=12)
+style.configure("TButton", font=font_button)
 gaussquality_gui(root)
 r = redirector()
 root.configure(background="#555555")
